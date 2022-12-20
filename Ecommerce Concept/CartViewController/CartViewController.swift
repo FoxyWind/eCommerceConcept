c20e3ee7b903e0fe1439b6d89caa8cb45f47f3c5
FoxyWind <ponomarevoleg89@gmail.com>
2022-12-21 00:19:59 +0500

    dev

--- Ecommerce Concept/CartViewController/CartViewController.swift
+++ Ecommerce Concept/CartViewController/CartViewController.swift
@@ -1,12 +1,16 @@
 import UIKit
 
 class CartViewController: UIViewController {
-
     @IBOutlet var addAddressView: UIView!
     @IBOutlet var addAddressImageView: UIImageView!
     @IBOutlet var backButtonView: UIView!
+    @IBOutlet var cartCollectionView: UIView!
     @IBOutlet var backButtonImageView: UIImageView!
+    @IBOutlet var cartInfoView: UIView!
     @IBOutlet var cartView: UIView!
+    @IBOutlet var totalAmountLabel: UILabel!
+    @IBOutlet var deliveryAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -15,8 +19,11 @@ class CartViewController: UIViewController {
 
     private func setupUI() {
         cartView.layer.cornerRadius = 25
+        cartCollectionView.layer.cornerRadius = 25
         backButtonImageView.image = UIImage(named: "Back")
         addAddressImageView.image = UIImage(named: "Location")
+        cartInfoView.layer.borderWidth = 1
+        cartInfoView.layer.borderColor = UIColor.gray.cgColor
     }
 
 
