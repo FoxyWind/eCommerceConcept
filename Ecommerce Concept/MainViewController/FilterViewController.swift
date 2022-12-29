223233fe633c3cf4ac3a88819b4535dfce988892
FoxyWind <ponomarevoleg89@gmail.com>
2022-12-29 19:17:34 +0500

    dev

--- Ecommerce Concept/MainViewController/FilterViewController.swift
+++ Ecommerce Concept/MainViewController/FilterViewController.swift
@@ -3,10 +3,12 @@ import UIKit
 class FilterViewController: UIViewController {
     private var projectColors = ProjectColors()
     @IBOutlet private var brandTableView: UITableView!
-    @IBOutlet private var xButtonView: UIButton!
     @IBOutlet private var brandTextField: UITextField!
+    @IBOutlet private var priceTableView: UITableView!
     @IBOutlet private var priceTextField: UITextField!
+    @IBOutlet private var sizeTableView: UITableView!
     @IBOutlet private var sizeTextField: UITextField!
+    @IBOutlet private var xButtonView: UIButton!
     @IBOutlet private var doneButton: UIButton!
     var delegate: FilterViewDelegate?
     
@@ -19,12 +21,17 @@ class FilterViewController: UIViewController {
     @IBAction func doneButton(_ sender: Any) {
         delegate?.closeView(sender: self)
     }
+    @IBAction func closeButton(_ sender: Any) {
+        delegate?.closeView(sender: self)
+    }
     
     func setup() {
         brandTableView.isHidden = true
-        brandTextField.text = "Choose Brand"
-        priceTextField.text = "Choose price limit"
-        sizeTextField.text = "Choose size"
+        priceTableView.isHidden = true
+        sizeTableView.isHidden = true
+        brandTextField.text = "- - -"
+        priceTextField.text = "- - -"
+        sizeTextField.text = "- - -"
         doneButton.layer.cornerRadius = 20
         doneButton.backgroundColor = projectColors.redColor
         xButtonView.layer.cornerRadius = 10
